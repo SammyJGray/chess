e0280c15bb9916e13aa2f017950a37518996fcee
Sammy <sammyjarmangray@gmail.com>
2025-05-06 19:46:58 +0100

    Added chess pieces

--- global.js
+++ global.js
@@ -5,6 +5,17 @@ for (let row = 0; row < 8; row++){
 		const tile = document.createElement('div');
 		const isDark = (row+col) % 2 == 1;
 		tile.style.backgroundColor = isDark ? '#769656' : '#eeeed2';
+		tile.dataset.row = row; tile.dataset.col = col;
 		board.appendChild(tile);
 	}
 }
+
+function getTile(row,col){
+	return document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+}
+
+const tile = getTile(1,4);
+const piece = document.createElement('img');
+piece.src = 'assets/sprites/black-queen.svg';
+tile.appendChild(piece);
+
